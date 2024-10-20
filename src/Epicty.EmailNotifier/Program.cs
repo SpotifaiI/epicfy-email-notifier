ab5515e8834007bfa5501ecae23ad12389e61efa
tdayko <ryan.bromati@hotmail.com>
2024-10-20 18:42:12 -0300

    docs: add swagger documentation

--- src/Epicty.EmailNotifier/Program.cs
+++ src/Epicty.EmailNotifier/Program.cs
@@ -3,32 +3,54 @@ using Epicty.EmailNotifier;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
 const string uri = "api/v1/notifications/";
-app.MapPost(uri, HandleSendEmailNotification);
+app.MapPost(uri, HandleSendEmailNotification).WithOpenApi(x =>
+{
+    x.Summary = "Send notification of new registered idea!";
+    return x;
+});
+
+app.UseHttpsRedirection();
 app.UseCors(options => options
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader()
 );
 
+app.UseSwagger();
+app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Epicfy Email Notifier"));
+app.MapGet("/", () => Results.Redirect("/swagger")).ExcludeFromDescription();
+
 app.Run();
 
-#region [endpoints handlers]
+#region [methods]
 
-static IResult HandleSendEmailNotification(SendEmailDto sendEmailDto)
+static IResult HandleSendEmailNotification(SendEmailDto sendEmailDto, ILogger<Program> logger)
 {
-    var emailNotification = new EmailNotification()
-        .WithTargetEmail(sendEmailDto.TargetEmail.ToString())
-        .WithUserName(sendEmailDto.CreatedBy)
-        .WithIdeaTitle(sendEmailDto.Idea.Title)
-        .WithIdeaDescription(sendEmailDto.Idea.Description)
-        .WithCreatedAt(sendEmailDto.Idea.CreatedAt);
+    try
+    {
+        var emailNotification = new EmailNotification()
+            .WithTargetEmail(sendEmailDto.TargetEmail.ToString())
+            .WithUserName(sendEmailDto.CreatedBy)
+            .WithIdeaTitle(sendEmailDto.Idea.Title)
+            .WithIdeaDescription(sendEmailDto.Idea.Description)
+            .WithCreatedAt(sendEmailDto.Idea.CreatedAt);
 
-    emailNotification.Send();
-    return Results.Ok();
+        logger.LogInformation($"Notificação enviada com sucesso para: {sendEmailDto.TargetEmail}");
+        emailNotification.Send();
+
+        return Results.Ok();
+    }
+    catch (Exception error)
+    {
+        logger.LogError($"Erro ao enviar notificação: {error}");
+        return Results.BadRequest($"Erro ao enviar notificação: {error}");
+    }
 }
 
 #endregion
