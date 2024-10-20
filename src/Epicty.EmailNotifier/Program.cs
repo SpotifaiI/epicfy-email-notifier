f7b7680f0275a9bb1822c8cd3ff704a968e498b4
tdayko <ryan.bromati@hotmail.com>
2024-10-20 15:31:20 -0300

    refactor: remove unused swagger gen config

--- src/Epicty.EmailNotifier/Program.cs
+++ src/Epicty.EmailNotifier/Program.cs
@@ -1,17 +1,18 @@
 using Epicty.EmailNotifier;
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.UseHttpsRedirection();
-
 const string uri = "api/v1/notifications/";
 app.MapPost(uri, HandleSendEmailNotification);
+app.UseCors(options => options
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
 app.Run();
 
 #region [endpoints handlers]
@@ -19,11 +20,11 @@ app.Run();
 static IResult HandleSendEmailNotification(SendEmailDto sendEmailDto)
 {
     var emailNotification = new EmailNotification()
+        .WithTargetEmail(sendEmailDto.TargetEmail.ToString())
         .WithUserName(sendEmailDto.CreatedBy)
         .WithIdeaTitle(sendEmailDto.Idea.Title)
         .WithIdeaDescription(sendEmailDto.Idea.Description)
-        .WithCreatedAt(sendEmailDto.Idea.CreatedAt)
-        .WithTargetEmail(sendEmailDto.TargetEmail.ToString());
+        .WithCreatedAt(sendEmailDto.Idea.CreatedAt);
 
     emailNotification.Send();
     return Results.Ok();
